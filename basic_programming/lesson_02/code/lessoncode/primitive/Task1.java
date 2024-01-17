1c41a8245f4162de0a0bf6bdedd84144f392dded
StanislavPB <perminovsb@gmail.com>
2024-01-17 12:04:53 +0000

    add lesson 2

---
+++ basic_programming/lesson_02/code/lessoncode/primitive/Task1.java
@@ -0,0 +1,12 @@
+package primitive;
+
+public class Task1 {
+    public static void main(String[] args) {
+        byte varByte = 120;
+        int varInt = 1256256254;
+        double varDouble = 163.0;
+
+
+
+    }
+}
