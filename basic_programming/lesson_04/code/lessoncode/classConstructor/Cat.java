54b3dabc7717b68ed5ca0610591acacc299fb7f9
StanislavPB <perminovsb@gmail.com>
2024-01-19 10:19:52 +0000

    add lesson 4

---
+++ basic_programming/lesson_04/code/lessoncode/classConstructor/Cat.java
@@ -0,0 +1,17 @@
+package classConstructor;
+
+public class Cat {
+    int age;
+
+    public void catMeow(){
+        System.out.println("Meow!!!");
+    }
+
+    public void catBirthday(){
+        System.out.println("У моей кошки сегодня день рождения!");
+        System.out.println("Ей было " + age + " лет");
+        age = age + 1;
+        System.out.println("а теперь ей " + age);
+    }
+
+}
---
+++ basic_programming/lesson_04/code/lessoncode/classConstructor/CatDemo.java
@@ -0,0 +1,23 @@
+package classConstructor;
+
+public class CatDemo {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+
+        myCat.age = 5;
+        myCat.catMeow();
+        myCat.catBirthday();
+
+        System.out.println(myCat.age);
+
+        Cat mySecondCat = new Cat();
+        mySecondCat.age = 2;
+
+        System.out.println("У моей кошки сегодня день рождения!");
+        System.out.println("Ей было " + mySecondCat.age + " лет");
+        mySecondCat.age = mySecondCat.age + 1;
+        System.out.println("а теперь ей " + mySecondCat.age);
+
+        mySecondCat.catBirthday();
+    }
+}
