ab91e2578684e81c1a7bfa1e1a20e27c42bd73c1
StanislavPB <perminovsb@gmail.com>
2024-01-22 10:03:16 +0000

    add lesson 5

---
+++ basic_programming/lesson_05/code/lessoncode/multiCooker/MultiCooker.java
@@ -0,0 +1,50 @@
+package multiCooker;
+
+public class MultiCooker {
+
+    String producer;
+    String model;
+    int power;
+    int volume;
+
+    String dish;
+    int currentProgramNumber;
+    int programNumbers;
+    String nameProgram;
+    int timeForProgram;
+
+    public MultiCooker(String producer, String model, int power, int volume) {
+        this.producer = producer;
+        this.model = model;
+        this.power = power;
+        this.volume = volume;
+    }
+
+    public void changeProgram(int newProgramNumber, String newProgram){
+        this.currentProgramNumber = newProgramNumber;
+        this.nameProgram = newProgram;
+        System.out.println("Программу готовки переключили на: " + nameProgram + "( программа номер: " + currentProgramNumber + ")");
+    }
+
+    public void changeDish(String newDish) {
+        this.dish = newDish;
+        System.out.println("В мультиварку поместили новые продукты для приготовления: " + dish + " в режиме: " + nameProgram);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "MultiCooker{" +
+                "producer='" + producer + '\'' +
+                ", model='" + model + '\'' +
+                ", power=" + power +
+                ", volume=" + volume +
+                ", dish='" + dish + '\'' +
+                ", currentProgramNumber=" + currentProgramNumber +
+                ", programNumbers=" + programNumbers +
+                ", nameProgram='" + nameProgram + '\'' +
+                ", timeForProgram=" + timeForProgram +
+                '}';
+    }
+}
---
+++ basic_programming/lesson_05/code/lessoncode/multiCooker/MultiCookerDemo.java
@@ -0,0 +1,37 @@
+package multiCooker;
+
+public class MultiCookerDemo {
+    public static void main(String[] args) {
+        MultiCooker mc1 = new MultiCooker("Tefal", "superModel", 800, 20);
+
+        System.out.println(mc1);
+
+
+        mc1.programNumbers = 3;
+
+        mc1.currentProgramNumber = 1;
+        mc1.nameProgram = "Тушение";
+        mc1.dish = "Кролик";
+        mc1.timeForProgram = 40;
+
+
+        System.out.println(mc1);
+
+        mc1.currentProgramNumber = 2;
+        mc1.nameProgram = "жарка";
+        System.out.println(mc1);
+
+        // --- если теперь мы будем использовать наши новые методы ---
+        System.out.println("==================== Используем методы =============");
+
+        mc1.changeProgram(1,"Тушение");
+        mc1.changeDish("Капуста");
+
+        mc1.changeProgram(3,"Выпекание");
+        mc1.changeDish("Тесто для шарлотки");
+
+        mc1.changeProgram(2, " Жарка");
+        mc1.changeDish("Куриные ножки");
+
+    }
+}
