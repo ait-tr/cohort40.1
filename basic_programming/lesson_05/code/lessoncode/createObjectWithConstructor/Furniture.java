c62869a071a5e2f8122e2eba411cc43cb407180a
StanislavPB <perminovsb@gmail.com>
2024-01-22 11:04:12 +0000

    add lesson 5

---
+++ basic_programming/lesson_05/code/lessoncode/createObjectWithConstructor/Furniture.java
@@ -0,0 +1,28 @@
+package createObjectWithConstructor;
+
+public class Furniture {
+
+    String type;
+
+    String color;
+
+    String material;
+
+    public Furniture(String type) {
+        this.type = type;
+    }
+
+    public Furniture(String type, String material ) {
+        this.type = type;
+        this.material = material;
+    }
+
+    @Override
+    public String toString() {
+        return "Furniture{" +
+                "type='" + type + '\'' +
+                ", color='" + color + '\'' +
+                ", material='" + material + '\'' +
+                '}';
+    }
+}
---
+++ basic_programming/lesson_05/code/lessoncode/createObjectWithConstructor/FurnitureExample.java
@@ -0,0 +1,20 @@
+package createObjectWithConstructor;
+
+public class FurnitureExample {
+    public static void main(String[] args) {
+        Furniture furniture1 = new Furniture("Table");
+
+        //... позже мы можем добавить данные в наши поля (переменные)
+
+        furniture1.material = "Wood";
+        furniture1.color = "White";
+
+        System.out.println(furniture1);
+
+        Furniture furniture2 = new Furniture("Table № 2", "Glass and metal");
+        furniture2.color = "Silver";
+
+        System.out.println(furniture2);
+
+    }
+}
