b42fc4420cccc235ba7323842f8d03234ae6de93
StanislavPB <perminovsb@gmail.com>
2024-01-22 11:29:49 +0000

    add lesson 5

--- basic_programming/lesson_05/code/lessoncode/metodsWithReturnTypeData/CalculatorWithReturnResult.java
+++ basic_programming/lesson_05/code/lessoncode/metodsWithReturnTypeData/CalculatorWithReturnResult.java
@@ -2,11 +2,13 @@ package metodsWithReturnTypeData;
 
 public class CalculatorWithReturnResult {
 
-
     public int sum(int a, int b){
         int result = a + b;
         return result;
     }
 // добавить три метода которые бы возвращали результат вычитания, умножения и деления
     // и использовать их
+
+
+
 }
