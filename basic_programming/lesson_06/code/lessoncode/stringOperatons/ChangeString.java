6b519e771395ca23e9fef253bbc4d0d57892c317
StanislavPB <perminovsb@gmail.com>
2024-01-23 12:02:50 +0000

    add lesson 6

---
+++ basic_programming/lesson_06/code/lessoncode/stringOperatons/ChangeString.java
@@ -0,0 +1,17 @@
+package stringOperatons;
+
+public class ChangeString {
+    public static void main(String[] args) {
+        String str1 = "Первая строка";
+
+        String str2 = str1;
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+        str1 = " Это теперь измененная первая строка";
+
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
