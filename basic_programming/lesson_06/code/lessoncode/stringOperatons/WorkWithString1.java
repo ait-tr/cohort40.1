d0928c11e063e4ae60cb72181ff2e8bb67ffe7f1
StanislavPB <perminovsb@gmail.com>
2024-01-23 12:00:22 +0000

    add lesson 6

--- basic_programming/lesson_06/code/lessoncode/stringOperatons/WorkWithString1.java
+++ basic_programming/lesson_06/code/lessoncode/stringOperatons/WorkWithString1.java
@@ -4,7 +4,7 @@ public class WorkWithString1 {
     public static void main(String[] args) {
         String text = "Это текст для проверки работы методов типа String";
 
-        System.out.println(text.length());
+        System.out.println(text);
 
         System.out.println("Получение символа из строки по его индексу");
 
@@ -18,8 +18,15 @@ public class WorkWithString1 {
         // System.out.println(text.charAt(49)); - ошибка потому что
         // при длине строки в 49 символов индекс изменяется от 0 до 48
 
-        System.out.println(" получение ");
+        System.out.println(" получение подстроки из целого текста");
+        System.out.println(" возможно либо от одного индекса до другого ");
+        System.out.println(" либо от одного индекса до конца строки");
 
+        String substringText1 = text.substring(40);
+        String substringText2 = text.substring(0,12);
+
+        System.out.println(substringText1);
+        System.out.println(substringText2);
 
     }
 }
---
+++ basic_programming/lesson_06/code/lessoncode/stringOperatons/WorkWithString3.java
@@ -0,0 +1,27 @@
+package stringOperatons;
+
+public class WorkWithString3 {
+    public static void main(String[] args) {
+        String text = "Это текст для проверки работы методов типа String";
+
+        System.out.println(text);
+        System.out.println(" ====== проверка вхождения текста в другую строку  =====");
+
+        String testString1 = "рабо";
+
+        boolean resultCheckContains = text.contains(testString1);
+
+        System.out.println("Contains result is - " + resultCheckContains);
+
+        System.out.println("Contains word 'str' result is - " + text.contains("str"));
+
+        String textAfterUpperCase = text.toUpperCase();
+        System.out.println(textAfterUpperCase);
+
+        String textAfterLowerCase = text.toLowerCase();
+        System.out.println(textAfterLowerCase);
+
+        System.out.println("Contains word 'str' result is - " + textAfterLowerCase.contains("str"));
+
+    }
+}
