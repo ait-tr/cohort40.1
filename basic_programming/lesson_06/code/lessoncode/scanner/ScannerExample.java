2f202112ec83b0d7dbe4a41dd7475875bb12cb34
StanislavPB <perminovsb@gmail.com>
2024-01-23 10:02:44 +0000

    add lesson 6

---
+++ basic_programming/lesson_06/code/lessoncode/scanner/ScannerExample.java
@@ -0,0 +1,25 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter text message: ");
+        String textMessage = scanner.nextLine();
+        System.out.println("Your text is: " + textMessage);
+
+        System.out.println("Please enter integer number: ");
+        int intNumber = scanner.nextInt();
+
+        System.out.println("Your number is: " + intNumber);
+
+        System.out.println("Please enter double number: ");
+        double doubleNumber = scanner.nextDouble();
+        System.out.println("Your number is: " + doubleNumber);
+
+
+        scanner.close();
+    }
+}
