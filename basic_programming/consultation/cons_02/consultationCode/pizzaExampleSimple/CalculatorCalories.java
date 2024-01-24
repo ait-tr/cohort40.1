105d139f7d46d8bf501c233cc18438a4b9722832
StanislavPB <perminovsb@gmail.com>
2024-01-24 16:01:49 +0000

    add cons_02

--- basic_programming/consultation/cons_02/consultationCode/pizzaExampleSimple/CalculatorCalories.java
+++ basic_programming/consultation/cons_02/consultationCode/pizzaExampleSimple/CalculatorCalories.java
@@ -1,3 +1,5 @@
+package pizzaExampleSimple;
+
 public class CalculatorCalories {
     /*
 
---
+++ basic_programming/consultation/cons_02/consultationCode/pizzaWithClasses/Pizza.java
@@ -0,0 +1,18 @@
+package pizzaWithClasses;
+
+public class Pizza {
+
+    int diameter;
+    int caloriesInOneCm;
+
+    public Pizza(int diameter, int caloriesInOneCm) {
+        this.diameter = diameter;
+        this.caloriesInOneCm = caloriesInOneCm;
+    }
+
+    public double totalCaloriesInPizza(){
+        double caloriesPizza = caloriesInOneCm * Math.PI * (diameter * diameter) / 4;
+        return caloriesPizza;
+    }
+
+}
