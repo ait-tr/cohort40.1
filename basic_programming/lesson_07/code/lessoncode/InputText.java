ed5a816998433a79705ecb9e6ddb1efe789be0ed
StanislavPB <perminovsb@gmail.com>
2024-01-24 09:59:44 +0000

    add lesson 7

---
+++ basic_programming/lesson_07/code/lessoncode/InputText.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InputText {
+
+    public String inputTextFromKeyboard(String message){
+        System.out.println(message);
+        Scanner scanner = new Scanner(System.in);
+        String userText = scanner.nextLine();
+
+      return userText;
+    }
+}
