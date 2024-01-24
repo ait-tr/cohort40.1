c2dea565b28cd21cedf4b2296c672ec9add6b2c5
StanislavPB <perminovsb@gmail.com>
2024-01-24 11:22:26 +0000

    add lesson 7

--- basic_programming/lesson_07/code/lessoncode/Hw_example1.java
+++ basic_programming/lesson_07/code/lessoncode/Hw_example1.java
@@ -18,7 +18,7 @@ public class Hw_example1 {
 
 
 Распечатать на консоль.
-
+1 Еще раз запросите у пользователя строку
 2 Напишите метод, который принимает в качестве параметра строку, передайте в этот метод строку, которую создали в п.1
 3 Распечатать последний символ строки. Используем метод String.charAt().
 4 Проверить, содержит ли ваша строка подстроку “Java”. Используем метод String.contains().
@@ -62,12 +62,7 @@ public class Hw_example1 {
 
         то есть при вызове метода substring(0,4) мы получим результат "0123"
 
-
-
-
        но если мы вызываем substring(5,9) мы получим результат "45678"
-
-
          */
 
         System.out.println(middleIndexFirstWord);
@@ -83,6 +78,31 @@ public class Hw_example1 {
         String mergeWord = firstPart + secondPart;
 
         System.out.println("Результат слияния: " + mergeWord) ;
+
+        System.out.println("====================");
+        System.out.println("Вторая часть задания");
+
+        /*
+        1 Еще раз запросите у пользователя строку
+2 Напишите метод, который принимает в качестве параметра строку, передайте в этот метод строку, которую создали в п.1
+3 Распечатать последний символ строки. Используем метод String.charAt().
+4 Проверить, содержит ли ваша строка подстроку “Java”. Используем метод String.contains().
+(5 Заменить все символы “а” на “о”.)
+6 Преобразуйте строку к верхнему регистру.
+7 Преобразуйте строку к нижнему регистру.
+8 Вырезать строку Java c помощью метода String.substring().
+         */
+
+        MethodsForWorkWithString methods = new MethodsForWorkWithString();
+
+        String textLine = sourceText.inputTextFromKeyboard("Введите строку текста:");
+
+        methods.lastLineSymbol(textLine);
+
+
+
+
+
 
     }
 }
---
+++ basic_programming/lesson_07/code/lessoncode/MethodsForWorkWithString.java
@@ -0,0 +1,28 @@
+public class MethodsForWorkWithString {
+
+    public void lastLineSymbol(String textLine) {
+        // вычисляем индекс последнего символа
+        int lastIndex = textLine.length() - 1;
+        // получаем последний символ по индексу
+        char lastSymbol = textLine.charAt(lastIndex);
+
+        System.out.println("Последний символ строки: " + lastSymbol);
+
+        System.out.println("Проверка содержания в нашей строке 'Java'");
+
+        String inOurLine = "Java";
+
+        boolean containsJava = textLine.contains(inOurLine);
+
+        System.out.println("Содержит ли наша строка текст 'Java' ?: " + containsJava);
+
+        String ourLineToUpperCase = textLine.toUpperCase();
+
+        String ourLineToLowCase = textLine.toLowerCase();
+
+        System.out.println("Наш текст в верхнем регистре: " + ourLineToUpperCase);
+
+        System.out.println("Наш текст в нижнем регистре: " + ourLineToLowCase);
+
+    }
+}
