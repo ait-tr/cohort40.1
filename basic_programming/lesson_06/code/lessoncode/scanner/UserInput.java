5b67f9995aecf2469bb82a3415fd20ea117d67c1
StanislavPB <perminovsb@gmail.com>
2024-01-25 09:00:38 +0000

    add lesson 08

--- basic_programming/lesson_06/code/lessoncode/scanner/UserInput.java
+++ basic_programming/lesson_06/code/lessoncode/scanner/UserInput.java
@@ -9,7 +9,6 @@ public class UserInput {
 
         System.out.println(message);
         String inputText = scanner.nextLine();
-        scanner.close();
         return inputText;
     }
     public int inputInteger(String message){
@@ -17,15 +16,12 @@ public class UserInput {
 
         System.out.println(message);
         int inputInteger = scanner.nextInt();
-        scanner.close();
         return inputInteger;
     }
     public double inputDouble(String message){
         Scanner scanner = new Scanner(System.in);
-
         System.out.println(message);
         Double inputDouble = scanner.nextDouble();
-        scanner.close();
         return inputDouble;
     }
 
