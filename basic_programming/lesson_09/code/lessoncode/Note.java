6f2a620b8bfec3bfe78bdd391c0b5700f200f753
StanislavPB <perminovsb@gmail.com>
2024-01-26 10:18:15 +0000

    add lesson 09

---
+++ basic_programming/lesson_09/code/lessoncode/Note.java
@@ -0,0 +1,18 @@
+public class Note {
+
+    private int id;
+    private String description;
+
+    public Note(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
