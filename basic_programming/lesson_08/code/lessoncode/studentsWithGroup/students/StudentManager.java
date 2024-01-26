6fd6aad29b8f6c4fb59ce722d88edd934958c60e
StanislavPB <perminovsb@gmail.com>
2024-01-26 09:28:14 +0000

    add lesson 08

--- basic_programming/lesson_08/code/lessoncode/studentsWithGroup/students/StudentManager.java
+++ basic_programming/lesson_08/code/lessoncode/studentsWithGroup/students/StudentManager.java
@@ -5,16 +5,12 @@ import scanner.UserInput;
 public class StudentManager {
 
 
-    private UserInput ui;
-
-    public StudentManager() {
-        this.ui = new UserInput();
-    }
+    private UserInput ui = new UserInput();
 
     public Group createGroup() {
         String groupName = ui.inputText("Название группы: ");
         String startDate = ui.inputText("Дата начала обучения: ");
-        int studentCount = Integer.parseInt(ui.inputText("Количество студентов в группе: "));
+        int studentCount = ui.inputInteger("Количество студентов в группе: ");
 
         return new Group(groupName, startDate, studentCount);
     }
