8660e4ce88fb6d66270f05283380165df42d9ce0
StanislavPB <perminovsb@gmail.com>
2024-01-29 15:30:16 +0000

    add consultation 3

---
+++ basic_programming/consultation/cons_03/consultationCode/code/Phone.java
@@ -0,0 +1,20 @@
+package code;
+
+public class Phone {
+
+    String model;
+
+    public Phone(String model) {
+        this.model = model;
+    }
+
+    public void printData(){
+        System.out.println("Phone model: " + model);
+    }
+
+    @Override
+    public String toString() {
+        return "Phone " +
+                "model: " + model;
+    }
+}
---
+++ basic_programming/consultation/cons_03/consultationCode/code/PhoneDemo.java
@@ -0,0 +1,10 @@
+package code;
+
+public class PhoneDemo {
+    public static void main(String[] args) {
+        Phone phone = new Phone("iPhone");
+
+        System.out.println(phone);
+        phone.printData();
+    }
+}
