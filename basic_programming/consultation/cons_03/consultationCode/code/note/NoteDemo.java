a6353099243e36541239e52e2e9f05d674043078
StanislavPB <perminovsb@gmail.com>
2024-01-29 15:33:52 +0000

    add consultation 3

---
+++ basic_programming/consultation/cons_03/consultationCode/code/note/NoteDemo.java
@@ -0,0 +1,16 @@
+package code.note;
+
+public class NoteDemo {
+    public static void main(String[] args) {
+        NoteManager manager = new NoteManager();
+
+        Note note1 = manager.createNote();
+        Note note2 = manager.createNote();
+        Note note3 = manager.createNote();
+
+        System.out.println(note1);
+        System.out.println(note2);
+        System.out.println(note3);
+
+    }
+}
---
+++ basic_programming/consultation/cons_03/consultationCode/code/note/NoteManager.java
@@ -0,0 +1,17 @@
+package code.note;
+
+import scanner.UserInput;
+
+public class NoteManager {
+
+    UserInput ui = new UserInput();
+
+
+    public Note createNote(){
+
+        return new Note(
+                ui.inputInteger("Введите номер заметки: "),
+                ui.inputText("Введите текст заметки: ")
+        );
+    }
+}
