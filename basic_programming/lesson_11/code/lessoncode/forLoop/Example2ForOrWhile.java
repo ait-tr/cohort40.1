fb982fbdb5400daea3380497c9447b9a4c092042
StanislavPB <perminovsb@gmail.com>
2024-01-30 11:39:49 +0000

    add lesson 11

---
+++ basic_programming/lesson_11/code/lessoncode/forLoop/Example2ForOrWhile.java
@@ -0,0 +1,36 @@
+package lesson_11.code.lessoncode.forLoop;
+
+import lesson_06.code.lessoncode.scanner.UserInput;
+
+public class Example2ForOrWhile {
+    public static void main(String[] args) {
+        /*
+        дан текст
+        вывести отдельно каждую букву этого текста
+         */
+
+        UserInput userInput = new UserInput();
+
+
+        String text = userInput.inputText("Введите текст:");
+
+        System.out.println("---------- Теперь цикл FOR -----------");
+
+        for (int i = 0; i < text.length(); i++) {
+            char currentChar = text.charAt(i);
+            System.out.println("Буква в тексте с индексом " + i + " : " + currentChar);
+        }
+
+        System.out.println("---------- Теперь цикл WHILE -----------");
+
+        int counter = 0;
+
+        while (counter < text.length()) {
+
+            char currentChar = text.charAt(counter);
+            System.out.println("Буква в тексте с индексом " + counter + " : " + currentChar);
+            counter++;
+        }
+
+    }
+}
