7130db137b7362aa803b50b5defc29942bf7a65c
StanislavPB <perminovsb@gmail.com>
2024-01-31 09:17:08 +0000

    add lesson 12

--- basic_programming/lesson_12/code/lessoncode/TaskAdd.java
+++ basic_programming/lesson_12/code/lessoncode/TaskAdd.java
@@ -9,6 +9,13 @@ public class TaskAdd {
 
     public static void main(String[] args) {
 
+        SimpleNumber simpleNumber = new SimpleNumber();
+
+        for (int i = 2; i < 100; i++) {
+            if (simpleNumber.isSimple(i)) {
+                System.out.println("Число " + i + " простое");
+            }
+        }
 
 
 
