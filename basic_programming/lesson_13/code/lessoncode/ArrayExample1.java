6e43f6d68c42b0302d53859b409c53c2be1152aa
StanislavPB <perminovsb@gmail.com>
2024-02-01 09:59:51 +0000

    add lesson 13

--- basic_programming/lesson_13/code/lessoncode/ArrayExample1.java
+++ basic_programming/lesson_13/code/lessoncode/ArrayExample1.java
@@ -1,7 +1,35 @@
 package lesson_13.code.lessoncode;
 
+import java.util.Arrays;
+
 public class ArrayExample1 {
     public static void main(String[] args) {
+        int[] array = new int[3];
+        array[0] = 6;
+        array[1] = 9;
+        array[2] = 6;
+
+        System.out.println(Arrays.toString(array));
+        System.out.println(array[0] + array[1] + array[2]);
+
+        array[0] = 10;
+        System.out.println(array[0] + array[1] + array[2]);
+
+        array[2] = 10;
+        System.out.println(array[0] + array[1] + array[2]);
+
+        System.out.println(array[0] == array[1]);
+        System.out.println(array[0] == array[2]);
+
+
+        String[] stringArray = {" word 1  ", "word 2", "word 3", "word 1"};
+
+        stringArray[0] = stringArray[0].trim();
+
+        System.out.println(Arrays.toString(stringArray));
+
+        System.out.println(stringArray[0].equals(stringArray[3]));
+
 
     }
 }
