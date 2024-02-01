3b0cfb9f0e75b8f1cdc1dfd65ae09f10502ac8b9
StanislavPB <perminovsb@gmail.com>
2024-02-01 12:00:52 +0000

    add lesson 13

---
+++ basic_programming/lesson_13/code/lessoncode/arrayWithMethods/ArrayService.java
@@ -0,0 +1,48 @@
+package lesson_13.code.lessoncode.arrayWithMethods;
+
+import lesson_06.code.lessoncode.scanner.UserInput;
+
+public class ArrayService {
+
+    public int[] createArray(){
+        UserInput userInput = new UserInput();
+
+        int lengthOurArray = userInput.inputInteger("введите длину массива");
+
+        int[] workingArray = new int[lengthOurArray];
+
+        return workingArray;
+
+    }
+
+    public void fillArray(int[] workingArray) {
+        UserInput userInput = new UserInput();
+
+        for (int i = 0; i < workingArray.length; i++) {
+            workingArray[i] = userInput.inputInteger("Введите значение " + i + "-ого элемента массива");
+        }
+    }
+
+    public int sumAllElements(int[] workingArray){
+        int sumAllElements = 0;
+
+        for (int i = 0; i < workingArray.length; i++) {
+            sumAllElements += workingArray[i];
+        }
+
+        return sumAllElements;
+    }
+
+    public int findMax(int[] workingArray){
+        int maxElementValue = workingArray[0];
+
+        for (int i = 1; i < workingArray.length; i++) {
+            if (workingArray[i] > maxElementValue) {
+                maxElementValue = workingArray[i];
+            }
+        }
+
+        return maxElementValue;
+    }
+
+}
--- basic_programming/lesson_13/code/lessoncode/arrayWithMethods/UseArrayForWorkWithMethods.java
+++ basic_programming/lesson_13/code/lessoncode/arrayWithMethods/UseArrayForWorkWithMethods.java
@@ -6,77 +6,31 @@ import java.util.Arrays;
 
 public class UseArrayForWorkWithMethods {
     public static void main(String[] args) {
-        /*
-        1) Создать массив той длины, которую введет пользователь
-        2) наполнить массив данными, которые введет пользователь
-        3) посчитать сумму всех элементов массива
-        4) найти максимальное значение среди элементов массива
+       /*
+       будем использовать методы для работы с массивами
 
+       создадим класс ArrayService
+       в котором создаим вспомогательные методы
 
-        для п1:
+       1) метод createArray - суть которого в создании нового массива
+       2) fillArray - заполнение массива
+       3) sumCalculate
+       4) findMax
+        */
 
-        - создать Сканер (или использовать наш класс UserInput)
-        - запросить у пользователя длину массива
-        - создать массив нужной длины
+        ArrayService service = new ArrayService();
 
-        для п2:
-        в цикле по длине массива для каждого элемента массива нужно
-        запросить у пользователя данные и сохранить то что он ввел
-        в соответствующей ячейке массива
+        int[] ourWorkingArray = service.createArray();
 
-        для п3:
-        объявляем переменную Sum в которой будет хранится сумма
-        всех элементов и присваиваем ей первоначальное значение 0
+        service.fillArray(ourWorkingArray);
 
-        в цикле по длине массива мы к этой переменной добавляем
-        значение i-ого элемента нашего массива
+        System.out.println(Arrays.toString(ourWorkingArray));
 
-        для п4:
-
-        объявляем переменную Max в которой будет хранится максимальное
-        значение из всех элементов и присваиваем ей первоначальное значение
-        из элемента с индексом 0 нашего массива
-
-        в цикле по всем элементам мы сравниваем каждый элемент со значением
-        которое хранится в переменной Max
-
-        если вдруг наш текущий элемент массива оказывается больше чем то значение
-        которое на текущий момент времени хранится в переменной Max, от это означает
-        что в переменную Max надо сохранить значение из текущего элемента массива
-
-         */
-
-        UserInput userInput = new UserInput();
-
-        int lengthOurArray = userInput.inputInteger("введите длину массива");
-
-        int[] workingArray = new int[lengthOurArray];
-
-        for (int i = 0; i < workingArray.length; i++) {
-            workingArray[i] = userInput.inputInteger("Введите значение " + i + "-ого элемента массива");
-        }
-
-        System.out.println(Arrays.toString(workingArray));
-
-        int sumAllElements = 0;
-
-        for (int i = 0; i < workingArray.length; i++) {
-            sumAllElements += workingArray[i];
-    // сокращенная форма записи, эквивалентно: sumAllElements = sumAllElements + workingArray[i];
-        }
+        int sumAllElements = service.sumAllElements(ourWorkingArray);
 
         System.out.println("Сумма всех элементов: " + sumAllElements);
 
-        int maxElementValue = workingArray[0];
-        // берем в качестве начального значения - значение первого элемента, а не 0
-        // потому что массив может содержать только отрицательные значения и тогда
-        // 0 всегда будет больше любого другого элемента
-
-        for (int i = 1; i < workingArray.length; i++) {
-            if (workingArray[i] > maxElementValue) {
-                maxElementValue = workingArray[i];
-            }
-        }
+       int maxElementValue = service.findMax(ourWorkingArray);
 
         System.out.println("Максимальное значение = " + maxElementValue);
 
