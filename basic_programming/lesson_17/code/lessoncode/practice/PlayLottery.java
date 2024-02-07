21a78b75c5dbcba3e72cfbeea3dde6b5a3067ff0
StanislavPB <perminovsb@gmail.com>
2024-02-07 11:27:57 +0000

    add lesson 17

--- basic_programming/lesson_17/code/lessoncode/practice/PlayLottery.java
+++ basic_programming/lesson_17/code/lessoncode/practice/PlayLottery.java
@@ -3,7 +3,7 @@ package lesson_17.code.lessoncode.practice;
 public class PlayLottery {
     public static void main(String[] args) {
         int lotterySize = 36;
-        int ticketSize = 5;
+        int winnerSize = 5;
 
 
         Lottery lottery = new Lottery();
@@ -12,11 +12,13 @@ public class PlayLottery {
 
 
         Player player1 = new Player("123-456-789",3);
-        player1.buyAnfFillTickets(ticketSize,lotterySize);
+        player1.buyAnfFillTickets(winnerSize,lotterySize);
 
         player1.printPlayerData();
 
         System.out.println("ПРОВОДИМ РОЗЫГРЫШ ЛОТЕРЕИ!");
+
+        lottery.lotteryRound(winnerSize);
 
         System.out.println("ВЫИГРЫШНЫЕ НОМЕРА:");
         lottery.getWinnerNumbers();
