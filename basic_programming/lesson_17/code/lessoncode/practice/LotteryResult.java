45079e12537eac5f1fc31cf9d2ebd331d60e97b8
StanislavPB <perminovsb@gmail.com>
2024-02-07 12:09:55 +0000

    add lesson 17

--- basic_programming/lesson_17/code/lessoncode/practice/LotteryResult.java
+++ basic_programming/lesson_17/code/lessoncode/practice/LotteryResult.java
@@ -1,9 +1,40 @@
 package lesson_17.code.lessoncode.practice;
 
+import java.util.Arrays;
+
 public class LotteryResult {
 
     public void checkLotteryResult(Lottery lottery, Player player){
         // провести проверку выигрышных номеров и сравнить с номерами в билетах игрока
+
+        // получаем выигрышные номера лотереи
+        int[] winnerNumbers = lottery.getWinnerNumbers();
+
+        Arrays.sort(winnerNumbers);
+
+        // Получить билеты от игрока
+
+        LotteryTicket[] tickets = player.getTickets();
+
+        System.out.println("Результат проверки для игрока: " + player.getIdCode());
+
+       // проводим проверку КАЖДОГО билета
+
+        for (int i = 0; i < tickets.length; i++) {
+            // получили номера из текущего билета
+            int[] numbersFromCurrentTicket = tickets[i].getTicketNumbers();
+
+            int matchCounter = 0;
+            // сравнить эти номера и выигрышные и сказать сколько совпадений
+
+            for (int j = 0; j < numbersFromCurrentTicket.length; j++) {
+                if (Arrays.binarySearch(winnerNumbers,numbersFromCurrentTicket[j]) >= 0) {
+                    matchCounter++;
+                }
+            }
+            // вывод количества совпадений в билете
+            System.out.println("Ticket № " + (i + 1) + " have " + matchCounter + " winner numbers");
+        }
 
     }
 }
--- basic_programming/lesson_17/code/lessoncode/practice/PlayLottery.java
+++ basic_programming/lesson_17/code/lessoncode/practice/PlayLottery.java
@@ -28,6 +28,5 @@ public class PlayLottery {
         lotteryResult.checkLotteryResult(lottery, player1);
 
 
-
     }
 }
