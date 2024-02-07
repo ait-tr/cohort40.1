b880fdf5a053e5c56ab0ff712eb99dfce7f77fa7
StanislavPB <perminovsb@gmail.com>
2024-02-07 11:29:29 +0000

    add lesson 17

---
+++ basic_programming/lesson_17/code/lessoncode/practice/LotteryResult.java
@@ -0,0 +1,9 @@
+package lesson_17.code.lessoncode.practice;
+
+public class LotteryResult {
+
+    public void checkLotteryResult(Lottery lottery, Player player){
+        // провести проверку выигрышных номеров и сравнить с номерами в билетах игрока
+
+    }
+}
