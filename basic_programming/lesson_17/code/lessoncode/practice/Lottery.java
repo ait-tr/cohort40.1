7d01315506a06e0447f3427d609ae1d927a2477a
StanislavPB <perminovsb@gmail.com>
2024-02-07 11:07:39 +0000

    add lesson 17

--- basic_programming/lesson_17/code/lessoncode/practice/Lottery.java
+++ basic_programming/lesson_17/code/lessoncode/practice/Lottery.java
@@ -1,7 +1,45 @@
 package lesson_17.code.lessoncode.practice;
 
+import java.util.Random;
+
 public class Lottery {
 
-    private
+    private int[] allLotteryNumbers;
+    private int[] winnerNumbers;
+
+    public int[] getAllLotteryNumbers() {
+        return allLotteryNumbers;
+    }
+
+    public int[] getWinnerNumbers() {
+        return winnerNumbers;
+    }
+
+    public void createLotteryNumbers(int sizeNumbers) {
+        allLotteryNumbers = new int[sizeNumbers];
+        for (int i = 0; i < allLotteryNumbers.length; i++) {
+            allLotteryNumbers[i] = (i + 1);
+        }
+    }
+
+    public void shuffleNumbers(){
+        Random random = new Random();
+        for (int i = allLotteryNumbers.length - 1; i > 0 ; i--) {
+            int index = random.nextInt(i + 1);
+            // change elements
+            int temp = allLotteryNumbers[index];
+            allLotteryNumbers[index] = allLotteryNumbers[i];
+            allLotteryNumbers[i] = temp;
+        }
+    }
+
+    public void lotteryRound(int quantityWinnerNumbers){
+        winnerNumbers = new int[quantityWinnerNumbers];
+        for (int i = 0; i < quantityWinnerNumbers; i++) {
+            winnerNumbers[i] = allLotteryNumbers[i];
+        }
+    }
+
+
 
 }
---
+++ basic_programming/lesson_17/code/lessoncode/practice/LotteryTicket.java
@@ -0,0 +1,39 @@
+package lesson_17.code.lessoncode.practice;
+
+import java.util.Random;
+
+public class LotteryTicket {
+    int[] ticketNumbers;
+
+    public void createAndFillTicket(int sizeTicket, int sizeLotteryNumbers){
+        ticketNumbers = new int[sizeTicket];
+        ticketNumbers = fillTicket(sizeLotteryNumbers);
+    }
+
+    private void fillTicket(int sizeLotteryNumbers){
+        Random random = new Random();
+
+        for (int i = 0; i < ticketNumbers.length; i++) {
+
+            boolean isNotFillCurrentNumber = true;
+
+            while (isNotFillCurrentNumber) {
+                int currentRandomValue = random.nextInt(sizeLotteryNumbers + 1);
+
+                if (isRandomNumberNotRepeate(currentRandomValue)) {
+                    ticketNumbers[i] = currentRandomValue;
+                    isNotFillCurrentNumber = false;
+                }
+            }
+        }
+    }
+
+    private boolean isRandomNumberNotRepeate(int checkNumber){
+        for (int i = 0; i < ticketNumbers.length; i++) {
+            if (ticketNumbers[i] == checkNumber) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
