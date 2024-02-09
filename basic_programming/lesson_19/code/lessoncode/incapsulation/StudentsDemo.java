5619d9f4a225a90ea33e7d78621b37d7c8245733
StanislavPB <perminovsb@gmail.com>
2024-02-09 09:16:42 +0000

    add lesson 19

--- basic_programming/lesson_19/code/lessoncode/incapsulation/StudentsDemo.java
+++ basic_programming/lesson_19/code/lessoncode/incapsulation/StudentsDemo.java
@@ -2,7 +2,7 @@ package lesson_19.code.lessoncode.incapsulation;
 
 public class StudentsDemo {
     public static void main(String[] args) {
-        Student student = new Student(1, "Ruslan", "12345");
+        Student student = new Student(1, "Ruslan", "12345", "12345");
 
         student.printStudentData();
 
