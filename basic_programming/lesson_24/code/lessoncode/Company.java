bbe31069e9dae95e449379f3cb2ee3e7f2414c57
StanislavPB <perminovsb@gmail.com>
2024-02-16 09:26:06 +0000

    add lesson 24

---
+++ basic_programming/lesson_24/code/lessoncode/Company.java
@@ -0,0 +1,36 @@
+package lesson_24.code.lessoncode;
+
+public class Company {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(1,"Ruslan");
+        Employee employee2 = new Employee(2,"Oleg");
+        Employee employee3 = new Employee(3,"Victor");
+        Employee employee4 = new Employee(4,"Ekaterina");
+        Employee employee5 = new Employee(5,"Alina");
+        Employee employee6 = new Employee(6,"Ludmila");
+        Employee employee7 = new Employee(7,"Anna");
+        Employee employee8 = new Employee(8,"Evgeny");
+        Employee employee9 = new Employee(9,"Natalia");
+        Employee employee10 = new Employee(10,"Ilya");
+
+        Employee[] employees = {
+                employee1,employee2,
+                employee3,employee4,
+                employee5,employee6,
+                employee7,employee8,
+                employee9,employee10
+        };
+
+
+//        System.out.println(employees[0].name);
+//        System.out.println(employees[1].name);
+//        System.out.println(employees[2].name);
+////...
+
+
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i].name);
+        }
+
+    }
+}
---
+++ basic_programming/lesson_24/code/lessoncode/Employee.java
@@ -0,0 +1,19 @@
+package lesson_24.code.lessoncode;
+
+public class Employee {
+    int id;
+    String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
