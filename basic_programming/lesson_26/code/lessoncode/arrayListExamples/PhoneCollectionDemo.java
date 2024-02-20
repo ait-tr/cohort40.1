2b73d1e544128543047c7c584dca550f64ea7d54
StanislavPB <perminovsb@gmail.com>
2024-02-20 11:33:52 +0000

    add lesson_26

--- basic_programming/lesson_26/code/lessoncode/arrayListExamples/PhoneCollectionDemo.java
+++ basic_programming/lesson_26/code/lessoncode/arrayListExamples/PhoneCollectionDemo.java
@@ -1,5 +1,7 @@
 package lesson_26.code.lessoncode.arrayListExamples;
 
+import java.util.ArrayList;
+
 public class PhoneCollectionDemo {
     public static void main(String[] args) {
         /*
@@ -15,5 +17,30 @@ public class PhoneCollectionDemo {
          показать коллекцию
 
          */
+
+        ArrayList<Phone> phones = new ArrayList<>();
+
+        System.out.println(phones);
+        System.out.println(phones.size());
+
+        phones.add(new Phone(1,"iPhone13 Max Pro", "Apple", 1250.00));
+
+        System.out.println(phones);
+        System.out.println(phones.size());
+
+        phones.add(new Phone(2,"iPhone14 Max Pro", "Apple", 1350.00));
+        phones.add(new Phone(3,"iPhone15 Max Pro", "Apple", 1500.00));
+        phones.add(new Phone(4,"Galaxy", "Samsung", 1100.00));
+
+
+        System.out.println(phones);
+        System.out.println(phones.size());
+
+        System.out.println(phones.get(2));
+
+        phones.remove(2);
+        System.out.println(phones.get(2));
+        System.out.println(phones);
+
     }
 }
