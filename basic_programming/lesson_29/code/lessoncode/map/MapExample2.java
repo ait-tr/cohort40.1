7eee49235e39fd3fa9865d95277a80372dc088f3
StanislavPB <perminovsb@gmail.com>
2024-02-23 09:45:10 +0000

    add lesson 29

---
+++ basic_programming/lesson_29/code/lessoncode/map/MapExample2.java
@@ -0,0 +1,30 @@
+package lesson_29.code.lessoncode.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample2 {
+    public static void main(String[] args) {
+        Map<String, String> telBook = new HashMap<>();
+
+        telBook.put("Masha", "12345678");
+        telBook.put("Petya", "347634673");
+        telBook.put("Oleg", "3562562357");
+        telBook.put("Dima", "3474674");
+        telBook.put("Nicolay", "2474753457");
+
+        for (Map.Entry<String,String> entry : telBook.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+
+        for ( String key : telBook.keySet()) {
+            System.out.println("Ключ: " + key + ", Значение: " + telBook.get(key));
+        }
+
+
+        for ( String value : telBook.values()) {
+            System.out.println("Значение: " + value);
+        }
+
+    }
+}
