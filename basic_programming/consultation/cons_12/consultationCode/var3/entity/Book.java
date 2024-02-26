c76687597b9a713d7b3721077b0b82dda9e26cd6
StanislavPB <perminovsb@gmail.com>
2024-02-26 14:05:24 +0000

    add lesson 30

---
+++ basic_programming/consultation/cons_12/consultationCode/var3/entity/Book.java
@@ -0,0 +1,47 @@
+package consultation.cons_12.consultationCode.var3.entity;
+
+public class Book {
+    private Integer bookId;
+    private String isbn;
+    private String title;
+    private Author author;
+    private Integer readerId;
+
+    public Book(Integer bookId, String isbn, String title, Author author) {
+        this.bookId = bookId;
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Integer getReaderId() {
+        return readerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author=" + author +
+                ", readerId=" + readerId +
+                '}';
+    }
+}
---
+++ basic_programming/consultation/cons_12/consultationCode/var3/service/LibraryManagement.java
@@ -0,0 +1,34 @@
+package consultation.cons_12.consultationCode.var3.service;
+
+import consultation.cons_12.consultationCode.var3.entity.Book;
+import consultation.cons_12.consultationCode.var3.entity.Reader;
+
+import java.util.List;
+
+public class LibraryManagement {
+
+    private List<Book> books;
+    private List<Reader> readers;
+
+    public LibraryManagement(List<Book> books, List<Reader> readers) {
+        this.books = books;
+        this.readers = readers;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public List<Reader> getReaders() {
+        return readers;
+    }
+
+    public void addBook(Book newBook){
+        books.add(newBook);
+    }
+
+    public void addReader(Reader newReader){
+        readers.add(newReader);
+    }
+
+}
