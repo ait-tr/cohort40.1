6f83d6caafdc40d0b306f1d1c8bee72308a77dc3
StanislavPB <perminovsb@gmail.com>
2024-02-28 14:21:21 +0000

    add lesson 32

--- basic_programming/lesson_32/code/lessoncode/practice/Task1.java
+++ basic_programming/lesson_32/code/lessoncode/practice/Task1.java
@@ -20,30 +20,31 @@ public class Task1 {
             System.out.println(arrayElement + " is correct : " + isCorrectBracket(arrayElement));
         }
     }
-        public static boolean isCorrectBracket(String stringLine) {
-            Stack<Character> stack = new Stack<>();
-            for (char ch : stringLine.toCharArray()) {
-                // Помещаем в стек открывающие скобки
-                if (ch == '(' || ch == '{' || ch == '[') {
-                    stack.push(ch);
-                } else {
-                    // Если стек пуст при встрече закрывающей скобки, последовательность неверна
-                    if (stack.isEmpty()) {
-                        return false;
-                    }
-                    // Проверяем, соответствует ли верхний элемент стека текущей закрывающей скобке
-                    char top = stack.pop(); // Получаем и удаляем верхний элемент стека
 
-                    if ((ch == ')' && top != '(') ||
-                            (ch == '}' && top != '{') ||
-                            (ch == ']' && top != '[')) {
-                        return false; // Если не соответствует, последовательность неверна
-                    }
+    public static boolean isCorrectBracket(String stringLine) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : stringLine.toCharArray()) {
+            // Помещаем в стек открывающие скобки
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                // Если стек пуст при встрече закрывающей скобки, последовательность неверна
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                // Проверяем, соответствует ли верхний элемент стека текущей закрывающей скобке
+                char top = stack.pop(); // Получаем и удаляем верхний элемент стека
+
+                if ((ch == ')' && top != '(') ||
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '[')) {
+                    return false; // Если не соответствует, последовательность неверна
                 }
             }
-            // Если стек пуст после обработки всех символов, последовательность верна
-            return stack.empty();
         }
+        // Если стек пуст после обработки всех символов, последовательность верна
+        return stack.empty();
+    }
 
 // перепишите логику кода таким образом, чтобы в стек помещались не
     // открывающиеся скобки, а "ожидаемые" закрывающиеся
@@ -51,9 +52,9 @@ public class Task1 {
     public static boolean isCorrectBracketType2(String stringLine) {
 
         Map<Character, Character> map = new HashMap<>();
-        map.put('(',')');
-        map.put('{','}');
-        map.put('[',']');
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
 
         Stack<Character> stack = new Stack<>();
         for (char ch : stringLine.toCharArray()) {
@@ -69,7 +70,6 @@ public class Task1 {
         // Если стек пуст после обработки всех символов, последовательность верна
         return stack.empty();
     }
-
 
 
 }
