2e78cf1ed4bcd00ba8c956d8e6ed1931d4b47106
StanislavPB <perminovsb@gmail.com>
2024-03-01 09:57:33 +0000

    add lesson 33

--- basic_programming/lesson_34/code/lessoncode/MyPriorityQueueDemo.java
+++ basic_programming/lesson_34/code/lessoncode/MyPriorityQueueDemo.java
@@ -6,13 +6,15 @@ public class MyPriorityQueueDemo {
 
         priorityQueue.enqueue(5,20);
         priorityQueue.enqueue(6,10);
-        priorityQueue.enqueue(7,15);
+        priorityQueue.enqueue(7,25);
         priorityQueue.enqueue(8,2);
         priorityQueue.enqueue(1,25);
         priorityQueue.enqueue(2,1);
-        priorityQueue.enqueue(3,9);
+        priorityQueue.enqueue(3,25);
 
         System.out.println(priorityQueue);
+
+        System.out.println(priorityQueue.dequeue());
 
     }
 }
