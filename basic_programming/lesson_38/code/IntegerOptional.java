58be8015a45fb02645396314ab97c1ed4385607a
StanislavPB <perminovsb@gmail.com>
2024-03-07 08:39:04 +0000

    add lesson 38

---
+++ basic_programming/lesson_38/code/IntegerOptional.java
@@ -0,0 +1,14 @@
+package lesson_38.code;
+
+import java.util.Optional;
+
+public class IntegerOptional {
+
+    public static Integer integerOptional(Optional<Integer> optNumber){
+        if (optNumber.isPresent()) {
+            return optNumber.get() * 2;
+        } else {
+            return null;
+        }
+    }
+}
