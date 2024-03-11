e8958ff8fccd1527f886a3e5d587afca601a3965
StanislavPB <perminovsb@gmail.com>
2024-03-11 14:22:16 +0000

    add cons 15

---
+++ basic_programming/consultation/cons_15/consultationCode/IfElseExample.java
@@ -0,0 +1,15 @@
+package consultation.cons_15.consultationCode;
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int x = - 10;
+
+        if (x < 0) {
+            System.out.println("Число отрицательное");
+        }
+        if (x % 2 == 0) {
+            System.out.println("Число четное");
+        }
+
+    }
+}
