2b8a25ce8398b14d58f708200ca335897ea9adf0
StanislavPB <perminovsb@gmail.com>
2024-03-13 08:58:11 +0000

    add lesson 41

---
+++ basic_programming/lesson_41/code/streamPractice/FlatMapExample.java
@@ -0,0 +1,36 @@
+package lesson_41.code.streamPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FlatMapExample {
+    public static void main(String[] args) {
+        String[][] array = new String[][] {
+                {"Hello", "world"},
+                {"Hello", "Java"},
+                {"Hello", "student"},
+                {"Welcome", "to","Java"}
+        };
+
+        List<String> streamArray = Arrays.stream(array)
+                .flatMap(innerArray -> Arrays.stream(innerArray))
+                .distinct()
+                .sorted()
+                .toList();
+
+        System.out.println(streamArray);
+
+        long count = Arrays.stream(array)
+                .flatMap(Arrays::stream)
+                .count();
+
+        long countUniq = Arrays.stream(array)
+                .flatMap(Arrays::stream)
+                .distinct()
+                .count();
+
+
+
+
+    }
+}
---
+++ basic_programming/lesson_41/code/streamPractice/FootballTeam.java
@@ -0,0 +1,28 @@
+package lesson_41.code.streamPractice;
+
+public class FootballTeam {
+
+    private String name;
+    private int power;
+
+    public FootballTeam(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    @Override
+    public String toString() {
+        return "FootballTeam{" +
+                "name='" + name + '\'' +
+                ", power=" + power +
+                '}';
+    }
+}
