7ae3c87599f13c6f2933773ce073ff065c6969a5
StanislavPB <perminovsb@gmail.com>
2024-03-14 11:59:51 +0000

    add lesson 42

---
+++ basic_programming/lesson_42/code/javaInputOutput/FileSystemOperation.java
@@ -0,0 +1,41 @@
+package lesson_42.code.javaInputOutput;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class FileSystemOperation {
+    public static void main(String[] args) {
+
+        File file = new File("test.txt");
+
+        boolean isExist = file.exists();
+
+        System.out.println("Does file exist? " + isExist);
+
+        // ---------
+
+        File dir = new File("new_dir");
+        boolean wasCreate = dir.mkdir();
+
+        System.out.println("Directory was create? " + wasCreate);
+
+        // -------
+
+        File directory = new File("new_dir");
+        boolean isDir = directory.isDirectory();
+        System.out.println(isDir);
+
+        // ------
+//        boolean successDelete = directory.delete();
+//        System.out.println(successDelete);
+
+        String[] filenames = directory.list();
+
+        System.out.println(Arrays.toString(filenames));
+
+        File[] files = directory.listFiles();
+
+        System.out.println(Arrays.toString(files));
+
+    }
+}
